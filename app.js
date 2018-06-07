e56aaa8cb6a7b2e0192568e9835c4590451fdedc
Edwin Zhao <edwin.zhao@outlook.com>
2018-06-06 20:29:56 -0400

    Changed to EJS Structuring
    Changed EJS, installed packages

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-//Dependencies for our web server.
-var express = require('express');
-//body parser lets us receive JSON data in the body of POST requests.
-var bodyParser = require('body-parser');
-//path is helps us find the directory where our public assets are.
-var path = require('path');
-
-//Sets up web server.
-var app = express();
+var express     = require('express');
+    bodyParser  = require('body-parser');
+    path        = require('path');
+    app         = express();
 
 //Sets server port.
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
 //Gives us access the files in our `public` folder, so going to http://yourwebsite.com/ will open index.html, located in the `public` folder.
 app.use(express.static(path.join(__dirname, 'public')));
-		
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("*", function(req, res) {
+    res.render("error");
+})
 
 //Starts the app on the port we defined earlier.
 //To actually run the app, run `node index.js`
